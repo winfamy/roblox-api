e0cfdf21e8d446cae6ba9a0dd241735d50fbe6ec
Winfamy <infamy@unyx.io>
2017-01-01 16:58:56 -0500

    New API version

--- api/roblox/inventory.js
+++ api/roblox/inventory.js
@@ -8,52 +8,68 @@ var API = {
             var left = types.length;
             var inventory = [];
             var pages = [];
-            for (var i = 0; i < types.length; i++)
+            for (var i = 0; i < types.length; i++) {
+                pages[i] = [];
                 getPage(types[i], 1);
+            }
 
-            function getPage(type, num, maxpages, tries) {
-                request.get('https://www.roblox.com/Trade/inventoryhandler.ashx?userid=' + user_id.toString() + '&page=' + num.toString() + '&itemsPerPage=25&assetTypeId=' + type.toString(), {timeout:5000}, function(err, resp, body) {
-                    if(err) {
-                        if(tries == 5)
-                            return complete(type);
-                        return getPage(type, num, maxpages, (tries == undefined)?1:tries+1);
+            function getPage(type, num, maxpages, tries=1) {
+                request.get('https://www.roblox.com/Trade/inventoryhandler.ashx?userid=' + user_id + '&page=' + num.toString() + '&itemsPerPage=25&assetTypeId=' + type.toString(), {timeout:5000}, function(err, resp, body) {
+                if(err) {
+                    if(tries == 5) return complete(type);
+                    return getPage(type, num, maxpages, tries+1);
+                }
+
+                var json = JSON.parse(body);
+                if(!(json.msg == 'Inventory retreived!'))
+                    return complete(type);
+
+
+                if(num == 1) {
+                    var maxpages = Math.ceil(json.data.totalNumber/25);
+                    if(maxpages == 1)
+                    return push(json, function() { complete(type); });
+                    push(json, function() {
+                    for (var i = 2; i <= maxpages; i++) {
+                        pages[types.indexOf(type)].push(i);
+                        console.log(pages[types.indexOf(type)]);
+                        getPage(type, i)
                     }
-
-                    var json = JSON.parse(body);
-                    if(!(json.msg == 'Inventory retreived!'))
-                        return complete(type);
-
-                    if(num == 1) {
-                        var maxpages = Math.ceil(json.data.totalNumber/25);
-                        if(maxpages == 1)
-                            return push(json, function() { complete(type); });
-                        push(json, function() { getPage(type, num + 1, maxpages); });
-                    } else {
-                        if(num == maxpages)
-                            return push(json, function() { complete(type); });
-                        push(json, function() { getPage(type, num + 1, maxpages); });
-                    }
+                    });
+                } else {
+                    push(json, function() {
+                    pageComplete(type, num);
+                    });
+                }
                 });
             }
 
             function push(json, cb) {
                 var left_to_process = json.data.InventoryItems.length;
                 json.data.InventoryItems.forEach(function(item) {
-                    inventory.push({
-                        name: item.Name,
-                        rap: parseInt(item.AveragePrice),
-                        uaid: item.UserAssetID,
-                        link: item.ItemLink
-                    });
-                    if(!--left_to_process) {
-                        cb();
-                    }
+                inventory.push({
+                    name: item.Name,
+                    rap: parseInt(item.AveragePrice),
+                    uaid: item.UserAssetID,
+                    link: item.ItemLink
                 });
+                if(!--left_to_process) {
+                    cb();
+                }
+                });
+            }
+
+            function pageComplete(type, page) {
+                pages[types.indexOf(type)].splice(pages[types.indexOf(type)].indexOf(page), 1);
+                console.log(pages[types.indexOf(type)]);
+                if(!pages[types.indexOf(type)].length) {
+                    complete(type);
+                }
             }
 
             function complete(type) {
                 if(!--left) {
-                    resolve(inventory);
+                 resolve(inventory);
                 }
             }          
         })
@@ -62,15 +78,13 @@ var API = {
         return new Promise((resolve, reject) => {
             var owners = [];
             function getPage(asset_id, page_cursor, attempts) {
-                var url = "https://inventory.roblox.com/v1/assets/"+asset_id.toString()+"/owners?sortOrder=Asc&limit=100" + (page_cursor?"&cursor=" + page_cursor:"");
+                var url = "https://inventory.roblox.com/v1/assets/"+asset_id.toString()+"/owners?sortOrder=Asc&limit=100&" + (page_cursor == undefined)?"cursor=" + page_cursor:"";
                 Axios.get(url).then((response) => {
-                    console.log(owners.length);
                     return (response.data.nextPageCursor == null)
                         ? push(response.data, () => { resolve(owners); })
                         : push(response.data, () => { getPage(asset_id, response.data.nextPageCursor, 1); });
                 })
                 .catch((err) => {
-                    console.log(err);
                     return (attempts == 3) ? resolve(owners) : getPage(asset_id, page_cursor, attempts+1);
                 });
             }
@@ -78,19 +92,16 @@ var API = {
             function push(json, callback) {
                 var left_to_process = json.data.length;
                 json.data.forEach(function(item) {
-                    if(item.owner != null)
-                        owners.push({
-                            uaid: item.userAssetID,
-                            serial: item.serialNumber,
-                            owner: item.owner.userId
-                        });
+                    owners.push({
+                        uaid: item.userAssetID,
+                        serial: item.serialNumber,
+                        owner: item.owner.userId
+                    });
                     if(!--left_to_process) {
-                        callback();
+                        cb();
                     }
                 });
             }
-
-            getPage(asset_id, undefined, 1);
         });
     }
 }
